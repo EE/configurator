4e922f10107b7b60bc43f5695277ae98cbf391de
Tadek Teleżyński <tadekte@gmail.com>
2017-05-30 17:03:35 +0200

    create simple GET test

--- configurator/apps/resource/tests.py
+++ configurator/apps/resource/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import Client
+from .models import *
 
-# Create your tests here.
+
+class APITest(TestCase):
+
+    def setUp(self):
+        # Every test needs a client.
+        self.sr = StringResource.objects.create(
+            name="test", description="test descr", value="test2")
+        self.client = Client()
+        response = self.client.get('/api/resource/')
+        print(response.status_code)
+        print(response.content)
+
+    def test_resource_get(self):
+        # Issue a GET request.
+        response = self.client.get('/api/resource/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(
+            response.content, b'{"test":{"type":"string","value":"test2","description":"test descr"}}')
